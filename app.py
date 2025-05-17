dd9a090a4be311ee81b979f58b052eb6718f202f
Janne Tompuri <janne.tompuri@gmail.com>
2025-05-17 12:21:34 +0300

    Added routes for all pages

--- app.py
+++ app.py
@@ -6,3 +6,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/contests")
+def contests():
+    return render_template("contests.html")
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
+@app.route("/my_texts")
+def my_texts():
+    return render_template("my_texts.html")
+
+@app.route("/terms_of_use")
+def terms_of_use():
+    return render_template("terms_of_use.html")
