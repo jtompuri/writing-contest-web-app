04289255edf2baa695afc2d75a88d22680019120
Janne Tompuri <janne.tompuri@gmail.com>
2025-05-18 18:02:39 +0300

    Updated create_user() and added user_count()

--- users.py
+++ users.py
@@ -1,20 +1,33 @@
 from werkzeug.security import check_password_hash, generate_password_hash
-
+import sqlite3
 import db
 
 def get_user(user_id):
-    sql = "SELECT id, username FROM users WHERE id = ?"
+    sql = "SELECT id, username, super_user FROM users WHERE id = ?"
     result = db.query(sql, [user_id])
     return result[0] if result else None
+
+
+def get_user_count():
+    query = "SELECT COUNT(*) FROM users"
+    result = db.query(query)
+    return result[0][0]
+
 
 def get_entries(user_id):
     sql = "SELECT id, contest_id, entry FROM entries WHERE user_id = ? ORDER BY id DESC"
     return db.query(sql, [user_id])
 
-def create_user(name, username, password):
+
+def create_user(name, username, password, is_super):
     password_hash = generate_password_hash(password)
-    sql = "INSERT INTO users (name, username, password_hash) VALUES (?, ?, ?)"
-    db.execute(sql, [username, password_hash])
+    query = "INSERT INTO users (name, username, password_hash, super_user) VALUES (?, ?, ?, ?)"
+    try:
+        db.execute(query, [name, username, password_hash, is_super])
+        return True
+    except sqlite3.IntegrityError:
+        return False
+
 
 def check_login(username, password):
     sql = "SELECT id, password_hash FROM users WHERE username = ?"
